7e962b1a5d926d9d832dddc84f0cb03d965b635c
Munene-Kariuki <denismunene999@gmail.com>
2022-05-24 14:57:02 +0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+export var questions = [
+  {
+    question: 'Who is the best kenyan president',
+    a: "Jomo Kenyatta",
+    b: "Daniel arap Moi",
+    c: "Mwai Kibaki",
+    d: "uhuru Kenyatta",
+    correct: "c"
+  },
+  {
+    question: "Who is the best artist?",
+    a: "Nyashinski",
+    b: 'Bahati',
+    c: "Pozze",
+    d: "Mejja",
+    correct: 'd'
+  },
+  {
+    question: "Which is the best movie?",
+    a: "Blacklist",
+    b: 'Money heist',
+    c: "Peaky blinders",
+    d: "Prison break",
+    correct: 'a'
+  },
+  {
+    question: "Which is the best sitcom?",
+    a: "How I met your mother",
+    b: 'Friends',
+    c: "Brooklyn nine nine",
+    d: "Neighborhood",
+    correct: 'b'
+  },
+  {
+    question: "Most boring subject?",
+    a: "English",
+    b: 'Swahili',
+    c: "Physics",
+    d: "Math",
+    correct: 'a'
+  },
+  {
+    question: "worst addiction?",
+    a: "Alcohol",
+    b: 'Women',
+    c: "Weed",
+    d: "Sheesha",
+    correct: ''
+  }
+];
---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+import { questions } from "./index";
+
+var form = document.getElementById("quiz");
+var question = document.getElementById("quest");
+var ansA = document.getElementById("a");
+var ansB = document.getElementById("b");
+var ansC = document.getElementById("c");
+var ansD = document.getElementById("d");
+
+var currentQuiz = 0;
+var score = 0;
+
+function loadQuiz() {
+
+  
+}
